62665a2fd4f71c76969d42f2b65d650f9fadee5b
Lance Alligood <lalligood@gmail.com>
2016-07-09 17:41:20 -0400

    With unified.conf for all maps, mapcrafter can now be run as a single command--no script needed

--- mapcrafter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# mapcrafter.sh is script for periodically updating Minecraft map
-# MCHOME is defined in .bashrc
-
-# Generate Overworld map
-mapcrafter -c $MCHOME/overworld.conf -j 2
-# Generate Nether map
-mapcrafter -c $MCHOME/nether.conf -j 2
